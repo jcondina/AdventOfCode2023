81d487aecf5d7ded3e4b69e70b08fd0af7b769ad
Juan I. Condina <juani.condina@gmail.com>
2023-12-18 04:40:09 -0300

    Add day03 part-2

--- day03/part-2.ts
+++ day03/part-2.ts
@@ -1,6 +1,5 @@
 import * as fs from "fs";
 
-const symbolsDictionary = "*+=-/%&#@$/";
 const numbersDictionary = "0123456789";
 const lineLength = 140;
 
@@ -14,7 +13,7 @@ export function solution(): number {
 		const indexes: number[] = [];
 
 		line.forEach((char, index) => {
-			if (symbolsDictionary.includes(char)) {
+			if (char === "*") {
 				indexes.push(index);
 			}
 		});
@@ -29,7 +28,7 @@ export function solution(): number {
 
 		while (isLeftSide ? i >= 0 : i < lineLength) {
 			if (inputLine[i] === ".") break;
-			if (symbolsDictionary.includes(inputLine[i])) break;
+			if (inputLine[i] === "*") break;
 
 			number = isLeftSide
 				? number + Number(inputLine[i]) * Math.pow(10, numberLength)
@@ -41,34 +40,39 @@ export function solution(): number {
 		if (number) numbers.push(number);
 	};
 
-	const getNumbersFromLine = (symbolIndex: number[], inputLine: string[]): number[] => {
+	const getNumbersFromSymbol = (index: number, inputLine: string[]): number[] => {
 		const numbers: number[] = [];
 
-		symbolIndex.forEach((index) => {
-			if (numbersDictionary.includes(inputLine[index])) {
-				let i = index - 1;
+		if (numbersDictionary.includes(inputLine[index])) {
+			let i = index - 1;
 
-				while (inputLine[i] !== "." && i >= 0) {
-					i--;
-				}
-
-				checkSide(i, inputLine, numbers);
-			} else {
-				checkSide(index, inputLine, numbers, true);
-				checkSide(index, inputLine, numbers);
+			while (inputLine[i] !== "." && i >= 0) {
+				i--;
 			}
-		});
+
+			checkSide(i, inputLine, numbers);
+		} else {
+			checkSide(index, inputLine, numbers, true);
+			checkSide(index, inputLine, numbers);
+		}
 
 		return numbers;
 	};
 
-	let numbers: number[] = [];
+	const numbers: number[] = [];
 
 	inputMatrix.forEach((line, index) => {
 		const lineIndexes = getSymbolsIndexes(line);
-		numbers = numbers.concat(getNumbersFromLine(lineIndexes, line));
-		if (index > 0) numbers = numbers.concat(getNumbersFromLine(lineIndexes, inputMatrix[index - 1]));
-		if (index < 140) numbers = numbers.concat(getNumbersFromLine(lineIndexes, inputMatrix[index + 1]));
+
+		lineIndexes.forEach((lineIndex) => {
+			let symbolNumbers: number[] = [];
+
+			symbolNumbers = symbolNumbers.concat(getNumbersFromSymbol(lineIndex, line));
+			if (index > 0) symbolNumbers = symbolNumbers.concat(getNumbersFromSymbol(lineIndex, inputMatrix[index - 1]));
+			if (index < 140) symbolNumbers = symbolNumbers.concat(getNumbersFromSymbol(lineIndex, inputMatrix[index + 1]));
+
+			if (symbolNumbers.length === 2) numbers.push(symbolNumbers[0] * symbolNumbers[1]);
+		});
 	});
 
 	return numbers.reduce((acc, curr) => acc + curr, 0);
